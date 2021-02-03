e858348b3075bcba306fad952ed2df676b39ea49
DelphicGen <gennardokidam@gmail.com>
2021-02-03 16:38:32 +0700

    Timeline1 responsiveness

--- src/components/Contribution.js
+++ src/components/Contribution.js
@@ -57,44 +57,46 @@ const Contribution = ({ commits }) => {
     }, [])
 
     return (
-        <div className="mt-10 border border-gray-400 pt-10 pl-5 pb-4 pr-4 w-fit-content mx-auto rounded-md flex">
-            <div className="flex flex-col text-white text-xs mr-5">
-                <span style={{width: '10px', height: '10px'}}></span>
-                <span className="mb-1">Mon</span>
-                <span style={{width: '10px', height: '10px'}}></span>
-                <span className="mb-1">Wed</span>
-                <span style={{width: '10px', height: '10px'}}></span>
-                <span className="mb-1">Fri</span>
-                <span style={{width: '10px', height: '10px'}}></span>
-            </div>
+        <div className="pt-10 pb-4 overflow-x-hidden ">
+            <div className="float-right flex">
+                <div className="flex flex-col text-white text-xs mr-5">
+                    <span style={{width: '10px', height: '10px'}}></span>
+                    <span className="mb-1">Mon</span>
+                    <span style={{width: '10px', height: '10px'}}></span>
+                    <span className="mb-1">Wed</span>
+                    <span style={{width: '10px', height: '10px'}}></span>
+                    <span className="mb-1">Fri</span>
+                    <span style={{width: '10px', height: '10px'}}></span>
+                </div>
 
-            <div>
-                {
-                    Object.keys(dateHelper).map((dateKey) => {
-                        return (
-                            <div key={dateKey} className="flex">
-                                {
-                                    dateHelper[dateKey].map((date, index) => {
-                                        if(date === firstD) gotFirstD = true
-                                        return (
-                                            <div key={date} className="relative">
-                                                {dateKey === '0' && (month[date.slice(5, 7)] !== prevMonth ? prevMonth = month[date.slice(5, 7)] : false) && <span className="text-xs text-white absolute" style={{top: '-180%'}} >{month[date.slice(5, 7)]}</span>}
-                                                <Rectangle empty={(index === 0 && date !== firstD && !gotFirstD)} freq={commits[date] ? commits[date].length : 0} date={date} />
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                        )
-                    })
-                }
-                <div className="flex justify-end items-center mt-2">
-                    <span className="text-white mr-3 text-sm">Less</span>
-                    <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-black`}></div>
-                    <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-300`}></div>
-                    <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-yellow-400`}></div>
-                    <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-400`}></div>
-                    <span className="text-white ml-3 text-sm">More</span>
+                <div>
+                    {
+                        Object.keys(dateHelper).map((dateKey) => {
+                            return (
+                                <div key={dateKey} className="flex">
+                                    {
+                                        dateHelper[dateKey].map((date, index) => {
+                                            if(date === firstD) gotFirstD = true
+                                            return (
+                                                <div key={date} className="relative">
+                                                    {dateKey === '0' && (month[date.slice(5, 7)] !== prevMonth ? prevMonth = month[date.slice(5, 7)] : false) && <span className="text-xs text-white absolute" style={{top: '-180%'}} >{month[date.slice(5, 7)]}</span>}
+                                                    <Rectangle empty={(index === 0 && date !== firstD && !gotFirstD)} freq={commits[date] ? commits[date].length : 0} date={date} />
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                    <div className="flex justify-end items-center mt-2">
+                        <span className="text-white mr-3 text-sm">Less</span>
+                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-black`}></div>
+                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-300`}></div>
+                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-yellow-400`}></div>
+                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-400`}></div>
+                        <span className="text-white ml-3 text-sm">More</span>
+                    </div>
                 </div>
             </div>
         </div>
