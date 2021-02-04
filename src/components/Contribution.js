295a815675fa6c098a99ae0a7782f831008e168d
DelphicGen <gennardokidam@gmail.com>
2021-02-04 21:55:10 +0700

    Change UI, add constants

--- src/components/Contribution.js
+++ src/components/Contribution.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState, useMemo } from 'react'
+import { month } from '../constants/constants'
 import Rectangle from './atomic/Rectangle';
 
 const Contribution = ({ commits }) => {
     const [dateHelper, setDateHelper] = useState({})
+    const [timeline, setTimeLine] = useState(null)
 
     const currentD = useMemo(() => new Date(), [])
     
@@ -18,21 +20,6 @@ const Contribution = ({ commits }) => {
 
     let prevMonth = ''
     let gotFirstD = false
-    
-    const month = {
-        '01': 'Jan',
-        '02': 'Feb',
-        '03': 'Mar',
-        '04': 'Apr',
-        '05': 'May',
-        '06': 'Jun',
-        '07': 'Jul',
-        '08': 'Aug',
-        '09': 'Sep',
-        '10': 'Oct',
-        '11': 'Nov',
-        '12': 'Des'
-    }
 
     useEffect(() => {
         
@@ -56,50 +43,80 @@ const Contribution = ({ commits }) => {
 
     }, [])
 
-    return (
-        <div className="pt-10 pb-4 overflow-x-hidden ">
-            <div className="float-right flex">
-                <div className="flex flex-col text-white mr-5" style={{ fontSize: '10px' }}>
-                    <span style={{width: '10px', height: '10px'}}></span>
-                    <span className="mb-1">Mon</span>
-                    <span style={{width: '10px', height: '10px'}}></span>
-                    <span className="mb-1">Wed</span>
-                    <span style={{width: '10px', height: '10px'}}></span>
-                    <span className="mb-1">Fri</span>
-                    <span style={{width: '10px', height: '10px'}}></span>
-                </div>
+    const getTimeLine = (date) => {
+        
+    //     axios
+    //         .get('https://api.github.com/search/commits?q=committer-name:DelphicGen committer-date:2021-02-03', {
+    //             "headers": {
+    //                 "Accept" : "application/vnd.github.cloak-preview"
+    //             }
+    //         })
+    //         .then(response => {
+    //         })
+        const tempTimeLine = []
+        commits[date].forEach(commit => {
+            tempTimeLine.push({time: commit.time.slice(11,19), message: commit.message})            
+        })
+        tempTimeLine.sort((a,b) => (a.time > b.time) ? -1 : ((b.time > a.time) ? 1 : 0))
+        console.log(tempTimeLine)
+    }
 
-                <div>
-                    {
-                        Object.keys(dateHelper).map((dateKey) => {
-                            return (
-                                <div key={dateKey} className="flex">
-                                    {
-                                        dateHelper[dateKey].map((date, index) => {
-                                            if(date === firstD) gotFirstD = true
-                                            return (
-                                                <div key={date} className="relative">
-                                                    {dateKey === '0' && (month[date.slice(5, 7)] !== prevMonth ? prevMonth = month[date.slice(5, 7)] : false) && <span className="text-xs text-white absolute" style={{ top: '-180%', fontSize: '10px' }} >{month[date.slice(5, 7)]}</span>}
-                                                    <Rectangle empty={(index === 0 && date !== firstD && !gotFirstD)} freq={commits[date] ? commits[date].length : 0} date={date} />
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                            )
-                        })
-                    }
-                    <div className="flex justify-end items-center mt-2">
-                        <span className="text-white mr-3 text-sm">Less</span>
-                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-black`}></div>
-                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-300`}></div>
-                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-yellow-400`}></div>
-                        <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-400`}></div>
-                        <span className="text-white ml-3 text-sm">More</span>
+    return (
+        <>
+            <div className="pt-10 pb-4 overflow-x-hidden overflow-y-hidden">
+                <div className="float-right flex">
+                    <div className="flex flex-col text-white mr-5" style={{ fontSize: '10px' }}>
+                        <span style={{width: '10px', height: '10px'}}></span>
+                        <span className="mb-1">Mon</span>
+                        <span style={{width: '10px', height: '10px'}}></span>
+                        <span className="mb-1">Wed</span>
+                        <span style={{width: '10px', height: '10px'}}></span>
+                        <span className="mb-1">Fri</span>
+                        <span style={{width: '10px', height: '10px'}}></span>
+                    </div>
+
+                    <div>
+                        {
+                            Object.keys(dateHelper).map((dateKey) => {
+                                return (
+                                    <div key={dateKey} className="flex">
+                                        {
+                                            dateHelper[dateKey].map((date, index) => {
+                                                if(date === firstD) gotFirstD = true
+                                                return (
+                                                    <div key={date} className="relative">
+                                                        {dateKey === '0' && (month[date.slice(5, 7)] !== prevMonth ? prevMonth = month[date.slice(5, 7)] : false) && <span className="text-xs text-white absolute" style={{ top: '-180%', fontSize: '10px' }} >{month[date.slice(5, 7)]}</span>}
+                                                        <Rectangle empty={(index === 0 && date !== firstD && !gotFirstD)} freq={commits[date] ? commits[date].length : 0} date={date} clickHandler={() => getTimeLine(date)} />
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                        <div className="flex justify-between items-center mt-4">
+                            <p className="text-white text-sm">Learn how we count contributions</p>
+                            <div className="flex items-center">
+                                <span className="text-white mr-3 text-sm">Less</span>
+                                <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-black`}></div>
+                                <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-300`}></div>
+                                <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-yellow-400`}></div>
+                                <div style={{width: '10px', height: '10px'}} className={`mx-1 bg-green-400`}></div>
+                                <span className="text-white ml-3 text-sm">More</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {
+                timeline && (
+                    <>
+
+                    </>
+                )
+            }
+        </>
     )
 }
 
--- src/components/atomic/Rectangle.js
+++ src/components/atomic/Rectangle.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { month } from '../../constants/constants'
 
-const Rectangle = ({ empty, freq, date }) => {
+const Rectangle = ({ empty, freq, date, clickHandler }) => {
+
+    const [stringDate, setStringDate] = useState('')
+    
+    useEffect(() => {
+        const tempDate = `${date.slice(8,10)} ${month[date.slice(5, 7)]}, ${date.slice(0,4)}`
+        setStringDate(tempDate)
+    }, [date])
+
     return (
         <div className="flex">
             {
-                empty && <div style={{width: '10px', height: '10px'}} className='mr-1 mb-1'>
-
-                </div>
+                empty && <div style={{width: '10px', height: '10px'}} className='mr-1 mb-1'></div>
             }
-            <div style={{width: '10px', height: '10px'}} className={`${freq === 0 ? 'bg-black' : (freq <= 5 ? 'bg-green-300' : freq <= 7 ? 'bg-yellow-400' : 'bg-green-400')} mr-1 mb-1 cursor-pointer relative group`}>
-                <p style={{ top: '150%', right: '-800%' }} className={`absolute text-xs z-50 bg-gray-900 rounded-md text-white text-center p-2 hidden group-hover:block w-40`}>
-                    { date }, { freq } {freq === 0 ? ' commit' : ' commits'}
+            
+            <div 
+                style={{width: '10px', height: '10px'}} 
+                className={`${freq === 0 ? 'bg-black' : (freq <= 5 ? 'bg-green-300' : freq <= 7 ? 'bg-yellow-400' : 'bg-green-400')} mr-1 mb-1 cursor-pointer relative group rounded-sm`} 
+                onClick={clickHandler}
+            >
+                <p 
+                    style={{ top: '150%', right: '-400%' }} 
+                    className={`absolute text-xs z-10 bg-gray-900 rounded-md text-white text-center p-2 hidden group-hover:block w-40`}
+                >
+                    { stringDate }, {freq === 0 ? ' no contribution' : ` ${freq} contribution(s)`}
                 </p>
             </div>
         </div>
